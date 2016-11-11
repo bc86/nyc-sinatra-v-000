cae5cc108066e437e091ba7df8538fa959bd699a
bc86 <michael.4486@gmail.com>
2016-11-11 15:50:55 -0500

    done

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,34 @@
 class LandmarksController < ApplicationController
 
+	get '/landmarks' do
+		@landmarks = Landmark.all
+		erb :"/landmarks/index"
+	end
+
+	get '/landmarks/new' do
+		
+		erb :"/landmarks/new"
+	end
+
+	post '/landmarks' do
+		@landmark = Landmark.find_or_create_by(params[:landmark])
+		redirect "/landmarks/#{@landmark.id}"
+	end
+
+	get "/landmarks/:id/edit" do
+		@landmark = Landmark.find_by_id(params[:id])
+		erb :"/landmarks/edit"
+	end
+
+	get "/landmarks/:id" do
+		@landmark = Landmark.find_by_id(params[:id])
+		erb :"/landmarks/show"
+	end
+
+	post "/landmarks/:id" do
+		@landmark = Landmark.find_by_id(params[:id])
+		@landmark.update(year_completed: params[:landmark][:year_completed], name: params[:landmark][:name])
+		redirect "figures/#{@figure.id}"
+	end	
+
 end
